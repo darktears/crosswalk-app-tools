6d001b7a32eedb0259ce6ff3c7e4aaff6f65cc42
Alexis Menard <alexis.menard@intel.com>
2016-07-14 05:30:35 -0700

    Windows : Improve the code a bit.

--- windows/lib/WinCentennial.js
+++ windows/lib/WinCentennial.js
@@ -20,14 +20,33 @@ Centennial.prototype.generateAppX =
 function(app_path, xwalk_path, meta_data, callback) {
     var basename = meta_data.product + "-" + meta_data.version;
     this._output.info(basename + '-appx');
+    this.runDesktopConverter(basename, meta_data, function(success) {
+        if (success) {
+            // Pass back built package
+            meta_data.appx = path.resolve(basename + ".appx");
+            // Only delete on success, for debugging reasons.
+            ShellJS.rm("-f", basename + "-appx");
+        }
+        callback(success);
+    });
+};
+
+Centennial.prototype.runDesktopConverter =
+function(basename, meta_data, callback) {
+
+    // To be used for cmd line arguments.
+    function InQuotes(arg) {
+        return "\"" + arg + "\"";
+    }
+
     var converter = "DesktopAppConverter.cmd -Installer " + meta_data.msi +
-        " -InstallerArguments \"/S\"" +
+        " -InstallerArguments " + InQuotes('/S') +
         " -Destination " + basename + '-appx' +
         " -Version " + meta_data.version +
-        " -Publisher \"CN=" + meta_data.manufacturer + "\"" +
-        " -PackageName \"" + meta_data.app_name + "\"" +
-        " -AppDisplayName \"" + meta_data.app_name + "\"" +
-        " -PackagePublisherDisplayName \"" + meta_data.manufacturer + "\"" +
+        " -Publisher " + InQuotes("CN=" + meta_data.manufacturer) +
+        " -PackageName " + InQuotes(meta_data.app_name) +
+        " -AppDisplayName " + InQuotes(meta_data.app_name) +
+        " -PackagePublisherDisplayName " + InQuotes(meta_data.manufacturer) +
         " -MakeAppx";
     this._output.info("Running '" + converter + "'");
     var child = child_process.exec(converter);
@@ -36,14 +55,15 @@ function(app_path, xwalk_path, meta_data, callback) {
         this.onData(data);
     }.bind(this));
 
+    child.stderr.on("data", function(data) {
+        this._output.warning(data);
+    }.bind(this));
+
     child.on("exit", function(code, signal) {
         if (code) {
             this._output.error("Unhandled error " + code);
         }
         callback(code === 0);
-        // Only delete on success, for debugging reasons.
-        ShellJS.rm("-f", basename + "-appx");
-        meta_data.appx = path.resolve(basename + ".appx");
         return;
     }.bind(this));
 };
